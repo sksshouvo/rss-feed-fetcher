466d3f156b3fe17a88adbf5b329e1eaa71329112
Salman Kabir <sksshouvo2@gmail.com>
2023-10-03 02:05:57 +0600

    Working on database

--- classes/rss_feed.py
+++ classes/rss_feed.py
@@ -1,5 +1,5 @@
 import json
-import feedparser
+import sqlite3
 from tkinter import messagebox
 from classes.validation import Validation
 
@@ -12,3 +12,14 @@ class rss_feed_class:
         rss_feed_link_validation = Validation.rss_feed_link_validation(rss_feed_link)
         print('Number of posts in RSS feed :', len(rss_feed_link_validation))
         print(json.dumps(rss_feed_link_validation, indent=4))
+        return json.dumps(rss_feed_link_validation)
+
+    @staticmethod
+    def store_rss_feed():
+        try:
+            sqlite3.connect('test.db')
+            print("Opened database successfully")
+        except ValueError as e:
+            # Display an error message when validation fails
+            messagebox.showerror("Error", str(e))
+        pass
